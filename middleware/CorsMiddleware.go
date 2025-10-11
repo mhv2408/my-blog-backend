ad08e5cdb6e53f8e8cf827965b0934830064bc05
Harsha Vardhan Mirthinti <mhv2408@gmail.com>
2025-10-11 11:32:38 -0700

    updated CORS allowed origins

--- middleware/CorsMiddleware.go
+++ middleware/CorsMiddleware.go
@@ -7,7 +7,7 @@ import (
 func CorsMiddleware(next http.Handler) http.Handler {
 	// set all the CORS permissions for the pre-flight responses
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Access-Control-Allow-Credentials", "true") //for the tokens
