ef45a80372866dbf63bc17f3f9a4fe9e8b7ffee7
Harsha Vardhan Mirthinti <mhv2408@gmail.com>
2025-10-17 01:55:56 -0700

    cors:added env for testing and production

--- middleware/CorsMiddleware.go
+++ middleware/CorsMiddleware.go
@@ -4,18 +4,32 @@ import (
 	"net/http"
 )
 
+var allowedOrigins = []string{
+	"http://localhost:5173",
+	"https://harsha-mirthinti.com",
+}
+
 func CorsMiddleware(next http.Handler) http.Handler {
 	// set all the CORS permissions for the pre-flight responses
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://harsha-mirthinti.com")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		w.Header().Set("Access-Control-Allow-Credentials", "true") //for the tokens
-		if r.Method == http.MethodOptions {
-			// it is just a preflight request
-			w.WriteHeader(http.StatusNoContent)
-			return
+		origin := r.Header.Get("Origin")
+		for _, allowedOrigin := range allowedOrigins {
+			if origin == allowedOrigin {
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+				w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+				w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+				w.Header().Set("Access-Control-Allow-Credentials", "true") //for the tokens
+				if r.Method == http.MethodOptions {
+					// it is just a preflight request
+					w.WriteHeader(http.StatusNoContent)
+					return
+				}
+				next.ServeHTTP(w, r) // does not return
+				return
+			}
 		}
-		next.ServeHTTP(w, r)
+		// No origin match, so forbidden request
+		http.Error(w, "Forbidden", http.StatusForbidden)
+
 	})
 }
